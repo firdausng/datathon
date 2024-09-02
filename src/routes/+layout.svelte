4f8709d5faa848d29994fd6feeba0611bdff20f4
Firdaus Kamaruddin <firdauskamaruddin@hotmail.com>
2024-09-03 06:23:50 +0800

    update ui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
     import Navbar from "$lib/ui/navbar.svelte";
 </script>
 
-<div class="container mx-auto text-primary-500">
+<div class="container mx-auto text-primary-500 px-4 md:px-2">
     <Navbar />
     <slot />
 </div>
